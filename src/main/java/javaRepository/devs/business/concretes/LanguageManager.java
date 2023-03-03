d872bb2fbfab7209ec7200a8771a8dd8092993f9
Çağatay Çelimli <c.celimli@live.co.uk>
2023-03-03 12:07:06 +0300

    Update

--- src/main/java/javaRepository/devs/business/concretes/LanguageManager.java
+++ src/main/java/javaRepository/devs/business/concretes/LanguageManager.java
@@ -10,7 +10,6 @@ import javaRepository.devs.utilities.mappers.ModelMapperService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,9 +27,6 @@ public class LanguageManager implements LanguageService {
 
     @Override
     public void add(CreateLanguageRequest createLanguageRequest) {
-//        Language language = new Language();
-//        language.setName(createLanguageRequest.getName());
-//        this.languageRepository.save(language);
         Language language = this.modelMapperService.forRequest().map(createLanguageRequest, Language.class);
         this.languageRepository.save(language);
 
@@ -39,13 +35,6 @@ public class LanguageManager implements LanguageService {
     @Override
     public List<GetAllLanguageResponse> getAll() {
         List<Language> languages = languageRepository.findAll();
-//        List<GetAllLanguageResponse> languageResponse = new ArrayList<GetAllLanguageResponse>();
-//        for (Language language : languages) {
-//            GetAllLanguageResponse responseItem = new GetAllLanguageResponse();
-//            responseItem.setId(language.getId());
-//            responseItem.setName(language.getName());
-//            languageResponse.add(responseItem);
-//        }
         List<GetAllLanguageResponse> languageResponse = languages.stream().map(language -> this.modelMapperService.forResponse().map(language, GetAllLanguageResponse.class)).collect(Collectors.toList());
         return languageResponse;
     }
@@ -59,16 +48,12 @@ public class LanguageManager implements LanguageService {
 
     @Override
     public void update(UpdateLanguageRequest updatelanguageRequest) {
-//        Language language = languageRepository.findById(updatelanguageRequest.getId()).get();
-//        language.setName(updatelanguageRequest.getName());
         Language language=this.modelMapperService.forRequest().map(updatelanguageRequest,Language.class);
         this.languageRepository.save(language);
     }
 
     @Override
     public void delete(DeleteLanguageRequest deleteLanguageRequest) {
-//        Language language = languageRepository.findById(deleteLanguageRequest.getId()).get();
-//        this.languageRepository.delete(language);
         Language language= this.modelMapperService.forRequest().map(deleteLanguageRequest,Language.class);
         this.languageRepository.deleteById(language.getId());
     }
